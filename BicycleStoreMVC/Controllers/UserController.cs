9f93c0eb1df5dded76002f3728cf4512ae82a627
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-11-14 14:20:07 +0100

    jfm file deletation

--- BicycleStoreMVC/Controllers/UserController.cs
+++ BicycleStoreMVC/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BicycleStoreMVC.Data;
+using BicycleStoreMVC.DTOs;
+using BicycleStoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,83 @@ namespace BicycleStoreMVC.Controllers
     /// </summary>
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Register(CustomerDto customerDto)
+        {
+            //check model validity
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //create password salt passwordhash
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash(customerDto.Password, out passwordHash, out passwordSalt);
+                    //convert customerDto to customer 
+                    var obj = new Customer
+                    {
+                        FirstName = customerDto.FirstName,
+                        LastName = customerDto.LastName,
+                        PasswordHash = passwordHash,
+                        PasswordSalt = passwordSalt,
+                        CreatedDate = DateTime.Now,
+                        Email = customerDto.Email,
+                        Street = customerDto.Street,
+                        City = customerDto.City,
+                        ZipCode = customerDto.ZipCode
+                    };
+
+
+                    //save to database
+                    _context.Add(obj);
+                    _context.SaveChanges();
+
+                    //to clear the form
+                    ModelState.Clear();
+                    //success msg
+                return PartialView("_RegisterSuccessful");
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception(ex.Message);
+                }
+
+
+
+            }
+            else
+            {
+                return View("Index");
+
+            }
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public IActionResult Login(CustomerDto customerDto)
+        {
+
         }
     }
 }
--- BicycleStoreMVC/DTOs/CustomerDto.cs
+++ BicycleStoreMVC/DTOs/CustomerDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,16 @@ namespace BicycleStoreMVC.DTOs
 {
     public class CustomerDto
     {
-        public int CustomerID { get; set; }
+        //public int CustomerID { get; set; }
+        [Required(ErrorMessage ="You cannot leave first name empty!")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "You cannot leave last name empty!")]
         public string LastName { get; set; }
-        public string UserName { get; set; }
+        [Required(ErrorMessage = "You cannot leave email adress empty!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "You cannot leave password empty!")]
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
